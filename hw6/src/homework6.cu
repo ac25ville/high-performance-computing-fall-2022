64c07195217088dea84b3a1dc058a418ea3acd41
Austin Corteville <acc9cm@DESKTOP-JD5P8BF.localdomain>
2022-12-11 15:20:19 -0600

    update

--- hw6/src/homework6.cu
+++ hw6/src/homework6.cu
@@ -251,7 +251,7 @@ grow_tubes(cntNode* readVector, const unsigned int readVectorSize, cntNode* shm,
     }
     */
     if(column < N){
-        printf("%d\n", column);
+        printf("%d\n", gridDim.x);
         cntNode newNode_a;
         newNode_a.x = readVector[readVectorSize-(column)].x;
         newNode_a.y = readVector[readVectorSize-(column)].y;
