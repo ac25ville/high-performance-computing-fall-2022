4d22c2dc8c83cac331e64e6d7114c277465a51b4
Austin Corteville <acc9cm@DESKTOP-JD5P8BF.localdomain>
2022-12-11 14:20:50 -0600

    update

--- hw6/src/homework6.cu
+++ hw6/src/homework6.cu
@@ -229,6 +229,7 @@ grow_tubes(cntNode* readVector, const unsigned int readVectorSize, cntNode* shm,
 
     const unsigned int column = (blockIdx.x * blockDim.x) + threadIdx.x;
     int row;
+    printf("%d\n", column);
     /*
     for(int j=0; j<2; j++){
         vector<cntNode> initTemp;
